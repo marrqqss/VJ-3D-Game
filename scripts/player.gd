46e30f13b13c57080255e08b9822089021c83509
Marc Ribas Acon <marcribasacon@gmail.com>
2025-04-29 13:30:22 +0200

    initial testing map and test player

---
+++ scripts/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+
+func _physics_process(delta: float) -> void:
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()

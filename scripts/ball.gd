5316da7563d63f14425f02139c63ca6408262f20
Marc Ribas Acon <marcribasacon@gmail.com>
2025-05-11 11:47:46 +0200

    blocks working fine and assets. ball starts with player

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -2,35 +2,62 @@ extends CharacterBody3D
 
 @export var SPEED: float = 20.0
 @export var BOUNCE: float = 1.0
+@export var attach_offset: Vector3 = Vector3(0, 0, -1)
 
-# Direction the ball is moving initialy
-var direction: Vector3 = Vector3(0.5, 0, 1)
+var launched: bool = false
+var direction: Vector3 = Vector3(0, 0, 1)
 
 func _ready() -> void:
-	# Initialize the built‑in velocity
-	velocity = direction * SPEED
 	add_to_group("ball")
+	velocity = Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
-	direction = direction.normalized()
-	velocity = direction * SPEED
-	move_and_slide()
+	if not launched:
+		
+		#lock to player
+		var paddle_transform = get_parent().global_transform
+		global_transform.origin = paddle_transform.origin + attach_offset
 
-	var col = get_last_slide_collision()
-	if col:
-		var collider = col.get_collider()
-		if collider.is_in_group("Player"): 
-			print("Player")
-			var local_hit_pos = col.get_position() - collider.global_transform.origin
-			var paddle_width = collider.get_node("CollisionShape3D").shape.extents.x
+		
+		if Input.is_action_just_pressed("ui_accept"):
+			#unlock from player
+			var was_global = global_transform
+			var root = get_tree().get_current_scene()
+			get_parent().remove_child(self)
+			root.add_child(self)
+			global_transform = was_global
+			launched = true
 
-			var factor = clamp(local_hit_pos.x / paddle_width, -1, 1)
-
-			direction.x = factor
-			direction.z = -abs(direction.z)
-
+			var move_input = Input.get_axis("ui_left", "ui_right")
+			if move_input < 0:
+				direction = Vector3(-1, 0, -1)
+			elif move_input > 0:
+				direction = Vector3( 1, 0, -1)
+			else:
+				direction = Vector3( 0, 0, -1)
 			direction = direction.normalized()
-		else:
-			direction = direction.bounce(col.get_normal()) * BOUNCE
+			velocity  = direction * SPEED
 
+
+	else:
+		direction = direction.normalized()
 		velocity = direction * SPEED
+		move_and_slide()
+
+		var col = get_last_slide_collision()
+		if col:
+			var collider = col.get_collider()
+			if collider.is_in_group("Player"):
+				# bounce against the player (controled by code)
+				var local_hit = col.get_position() - collider.global_transform.origin
+				var pad_shape = collider.get_node("CollisionShape3D").shape
+				var half_width = pad_shape.extents.x
+				var factor = clamp(local_hit.x / half_width, -1, 1)
+
+				direction.x = factor
+				direction.z = -abs(direction.z)
+				direction = direction.normalized()
+			else:
+				# bounce controled by physics
+				direction = direction.bounce(col.get_normal()) * BOUNCE
+				direction = direction.normalized()

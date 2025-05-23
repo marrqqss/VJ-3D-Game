978ce70823d090a3fb6ffd9a619d4c16268d35ce
Marc Ribas Acon <marcribasacon@gmail.com>
2025-05-23 21:27:41 +0200

    ball fixes

--- scripts/map_cinematic.gd
+++ scripts/map_cinematic.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @onready var camera_pivot: Node3D = $CameraPivot
 @onready var player: Node = $Player
+@onready var ball: CharacterBody3D = $Player/Ball
 
 var rotation_speed: float = 50
 var drop_height: float   = 15
@@ -9,6 +10,8 @@ var drop_height: float   = 15
 func _ready() -> void:
 	player.set_process(false)
 	player.set_physics_process(false)
+	ball.set_process(false)
+	ball.set_physics_process(false)
 
 	var start_angle = camera_pivot.rotation_degrees.y
 	var start_y = camera_pivot.position.y
@@ -35,3 +38,5 @@ func _animate_intro(start_angle: float, start_y: float) -> void:
 	camera_pivot.position.y          = start_y
 	player.set_process(true)
 	player.set_physics_process(true)
+	ball.set_process(true)
+	ball.set_physics_process(true)

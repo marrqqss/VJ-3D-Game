b022ecabddc33053fa1d710d12dae53fd691692d
Marc Ribas Acon <marcribasacon@gmail.com>
2025-05-30 21:34:11 +0200

    puntuation showing in titlescreen

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -2,6 +2,13 @@ extends VBoxContainer
 
 const map1 = preload("res://scenes/map1.tscn")
 
+func _process(delta: float) -> void:
+	$"../Puntuations2/puntuation1".text = str(GameState.map1_puntuation)
+	$"../Puntuations2/puntuation2".text = str(GameState.map2_puntuation)
+	$"../Puntuations2/puntuation3".text = str(GameState.map3_puntuation)
+	$"../Puntuations2/puntuation4".text = str(GameState.map4_puntuation)
+	$"../Puntuations2/puntuation5".text = str(GameState.map5_puntuation)
+
 func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_packed(map1)
 	pass
